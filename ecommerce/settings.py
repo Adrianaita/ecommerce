36b813f1c59927b225f345f7d153f0df98abfa0d
Adrianaita <adriana.giuffrida@edu.unifi.it>
2025-07-09 20:16:46 +0200

    Allowed hosts modificato

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-sostituiscimi'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
+ALLOWED_HOSTS = ['ecommerce-production-cdc6.up.railway.app', 'localhost', '127.0.0.1']
 
 
 # Application definition
